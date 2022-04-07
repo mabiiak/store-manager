c95adc77435e5ab3b161e6335c65d7c9a49231ae
mabiak <mabiane.polniak@gmail.com>
2022-04-06 22:16:52 -0300

    fix: teste getAllProductsModel

--- test/unit/models/productsModel.js
+++ test/unit/models/productsModel.js
@@ -1,27 +1,27 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const { execute } = require('../../../models/connection');
-
 const connection = require('../../../models/connection');
 
-const { getAllProductsModel } = require('../../../models/productsModel');
+const { getAllProductsModel, createNewProductModel } = require('../../../models/productsModel');
 
-after(async () => {
-  connection.execute.restore();
-});
+describe('Model Products', () => {
+  describe('getAllProducts', () => {
+    describe('quando a tabela `products` tiver dados', () => {
+      const produtosDuble = [
+        { id: 1, 'name': 'Martelo de Thor', 'quantity': 10 },
+        { id: 2, 'name': 'Traje de encolhimento', 'quantity': 20 },
+        { id: 3, 'name': 'Escudo do Capitão América', 'quantity': 30 }, 
+      ];
 
-describe('Testa Model Products', () => {
-  it('A rota "get/products" retorna um array', async () => {
-    const produtos = [
-      { id: 1, 'name': 'Martelo de Thor', 'quantity': 10 },
-      { id: 2, 'name': 'Traje de encolhimento', 'quantity': 20 },
-      { id: 3, 'name': 'Escudo do Capitão América', 'quantity': 30 }, 
-    ];
+      before(() => {
+        sinon.stub(connection, 'execute').resolves(produtosDuble)
+      })
 
-    sinon.stub(connection, 'execute').resolves(produtos);
-
-    const allProducts = await getAllProductsModel();
-
-    expect(allProducts).to.be.deep.eq(produtos)
-  })
+      it('retorna todos os dados', async () => {
+        const allProducts = await getAllProductsModel();
+        expect(allProducts).to.be.deep.eq(produtosDuble);
+      })
+    });
+  });
 })
