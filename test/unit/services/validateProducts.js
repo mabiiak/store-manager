6f6a232d624bd3503ec83d59d680034be526ea87
mabiak <mabiane.polniak@gmail.com>
2022-04-10 18:15:28 -0300

    test: add testes a validaçao

--- test/unit/services/validateProducts.js
+++ test/unit/services/validateProducts.js
@@ -20,4 +20,38 @@ describe('Validates Products', () => {
       expect(res.status.calledWith(422)).to.be.true;
     });
   })
+
+  describe('validateQuantityProduct', async () => {
+    const req = {
+      body: { quantity: -1 }
+    };
+    const res = {};
+
+    before(() => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub();
+    });
+
+    it('deve chamar a função `res.status` com o status 422', async () => {
+      await validate.validateQuantityProduct(req, res);
+      expect(res.status.calledWith(422)).to.be.true;
+    });
+  })
+
+  describe('checkProductNotExist', async () => {
+    const req = {
+      params: { id: 55 }
+    };
+    const res = {};
+
+    before(() => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub();
+    });
+
+    it('deve chamar a função `res.status` com o status 404', async () => {
+      await validate.checkProductNotExist(req, res);
+      expect(res.status.calledWith(404)).to.be.true;
+    });
+  })
 });
